25b9ce23b97941548dc4e27c3f4e2466003a9b38
Harish Pant <pantharish120@gmail.com>
2025-03-19 21:47:44 +0545

    :sparkles: Added BlockedIp model for managing blocked IPs

---
+++ src/Models/BlockedIp.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Harryes\SentinelLog\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlockedIp extends Model
+{
+    protected $table = 'sentinel_blocked_ips';
+
+    protected $fillable = [
+        'ip_address',
+        'blocked_at',
+        'expires_at',
+        'reason',
+    ];
+
+    protected $casts = [
+        'blocked_at' => 'datetime',
+        'expires_at' => 'datetime',
+    ];
+
+    /**
+     * Check if the IP is currently blocked.
+     */
+    public function isActive(): bool
+    {
+        return !$this->expires_at || $this->expires_at->isFuture();
+    }
+}
