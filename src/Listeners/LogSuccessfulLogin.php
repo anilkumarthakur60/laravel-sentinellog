09c1f060606a7f046af108a5dcba31ba72c720cb
Harish Pant <pantharish120@gmail.com>
2025-03-19 21:32:37 +0545

    :rocket: Updated LogSuccessfulLogin to log 2FA required events

--- src/Listeners/LogSuccessfulLogin.php
+++ src/Listeners/LogSuccessfulLogin.php
@@ -7,17 +7,23 @@ namespace Harryes\SentinelLog\Listeners;
 use Harryes\SentinelLog\Models\AuthenticationLog;
 use Harryes\SentinelLog\Services\DeviceFingerprintService;
 use Harryes\SentinelLog\Services\GeolocationService;
+use Harryes\SentinelLog\Services\TwoFactorAuthenticationService;
 use Illuminate\Auth\Events\Login;
 
 class LogSuccessfulLogin
 {
     protected DeviceFingerprintService $fingerprintService;
     protected GeolocationService $geoService;
+    protected TwoFactorAuthenticationService $twoFactorService;
 
-    public function __construct(DeviceFingerprintService $fingerprintService, GeolocationService $geoService)
-    {
+    public function __construct(
+        DeviceFingerprintService $fingerprintService,
+        GeolocationService $geoService,
+        TwoFactorAuthenticationService $twoFactorService
+    ) {
         $this->fingerprintService = $fingerprintService;
         $this->geoService = $geoService;
+        $this->twoFactorService = $twoFactorService;
     }
 
     public function handle(Login $event): void
@@ -36,6 +42,19 @@ class LogSuccessfulLogin
             'location' => $this->geoService->getLocation(request()->ip()),
             'is_successful' => true,
         ]);
+
+        if ($event->user->two_factor_secret && !$event->user->session()->has('2fa_verified')) {
+            AuthenticationLog::create([
+                'authenticatable_id' => $event->user->getKey(),
+                'authenticatable_type' => get_class($event->user),
+                'event_name' => '2fa_required',
+                'ip_address' => request()->ip(),
+                'user_agent' => request()->userAgent(),
+                'device_info' => $this->fingerprintService->generate(),
+                'location' => $this->geoService->getLocation(request()->ip()),
+                'is_successful' => false,
+            ]);
+        }
 
         if ($event->user->isNewDevice($log->device_info['hash'] ?? '')) {
             $event->user->notifyNewDevice($log);
