bdecd9858dc717a363d3ed93634324f7e9596fbe
Harish Pant <pantharish120@gmail.com>
2025-03-21 21:49:19 +0545

    :hammer: Query updates for fetching new device

--- src/Services/DeviceFingerprintService.php
+++ src/Services/DeviceFingerprintService.php
@@ -41,7 +41,7 @@ class DeviceFingerprintService
         return !AuthenticationLog::where('authenticatable_id', $user->getKey())
             ->where('authenticatable_type', get_class($user))
             ->where('is_successful', true)
-            ->whereJsonContains('device_info->hash', $hash)
+            ->whereRaw("JSON_CONTAINS(device_info->'$.hash', ?)", [$hash])
             ->exists();
     }
 
